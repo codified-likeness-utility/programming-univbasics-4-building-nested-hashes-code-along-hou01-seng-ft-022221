a33fc1c59d6905321bae705df0c2d0f825a17b93
Ian Davies <mr.daviesian@gmail.com>
2021-01-25 17:30:27 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,11 +1,18 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
+      :patriarch => {},
+      :matriarch => {},
+      :helper_method => {},
+      :hero_friends => []
       }, 
     :capulet => {
-      #your key/value pairs here!
-      }}
+      :patriarch => {},
+      :matriarch => {},
+      :helper_method => {},
+      :hero_friends => []
+      }
+  }
 
   
 end
